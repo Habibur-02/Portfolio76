719d7db89537c517483458c01e88697f7eb7e30b
Habibur Rahman Aasif <156560375+Habibur-02@users.noreply.github.com>
2025-03-23 17:37:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,13 +77,7 @@ export default function App() {
             >
               <FaLinkedin className="social-icon" />
             </a>
-                <a
-  href="mailto:habibur.ruet10@gmail.com"
-  target="_blank"
-  rel="noreferrer"
->
-  <FaEnvelope className="social-icon" />
-</a>
+          
           </div>
         </Container>
       </Navbar>
